e4ff39694e679b5e7925e945039d7d532de96e0b
Daniel117-dev <danieltesfay7735@gmail.com>
2025-03-10 00:42:13 +0300

    updated import.py

--- import.py
+++ import.py
@@ -11,5 +11,5 @@ with open("biolink_model.yaml", "r", encoding="utf-8") as file:
 
     data = yaml.safe_load(file)
     print(data)
-    with open('example2.txt', 'w') as file2:
+    with open('example.txt', 'w') as file2:
        file2.write(json.dumps(data))
